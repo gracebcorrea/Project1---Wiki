cb8d558a451c5d032d07ef3b69f049c8f22ee853
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-07-02 12:17:33 -0300

    Update urls.py

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path('admin/', admin.site.urls),
+    path("NewPage", views.add, name="NewPage"),
 ]
