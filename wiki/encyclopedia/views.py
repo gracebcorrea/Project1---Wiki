1350d56f457ec1057a793855d3e1fa2b833f0f61
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-07-02 14:40:01 -0300

    Update views.py

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -5,12 +5,32 @@ from django.urls import reverse
 
 from . import util
 
+class NewPageEntry(forms.Form):
+    page = forms.CharField(label="New Page")
+
 
 def index(request):
-    if "encyclopedia" not in request.session:
-        request.session["encyclopedia"] = []
+    if "Pwiki" not in request.session:
+        request.session["Pwiki"] = []
 
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries() ,
-        "encyclopedia": request.session["encyclopedia"]
+        "encyclopedia": request.session["Pwiki"]
     })
+
+
+def add(request):
+    if request.method == "POST":
+        form = NewPageEntry(request.POST)
+        if form.is_valid():
+            page = form.cleaned_data["Pwiki"]
+            request.session["Pwiki"] += [page]
+            return HttpResponseRedirect(reverse("Pwiki:index"))
+        else:
+            return render(request, "Pwiki/NewPage.html", {
+                "form": form
+            })
+    else:
+        return render(request, "Pwiki/NewPage.html", {
+            "form": NewPageEntry()
+        })
