e756b4ae6ef9f6f817ba3023ac58a84fa7f294b9
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-07-02 14:20:07 -0300

    Update views.py

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from . import util
 
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
+    if "encyclopedia" not in request.session:
+        request.session["encyclopedia"] = []
 
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries() ,
+        "encyclopedia": request.session["encyclopedia"]
+    })
