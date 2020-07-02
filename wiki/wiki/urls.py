3d7f86e57c709417ef8ca7cad4cabc6530fa25b8
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-07-02 19:01:13 -0300

    Update urls.py

--- wiki/wiki/urls.py
+++ wiki/wiki/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("encyclopedia.urls"))
+    path('', include("encyclopedia.urls")),
+    #path('',include("entries.urls")),
 ]
