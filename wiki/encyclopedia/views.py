05b7b9dc3ec9d7b35473b5db122e50861259195b
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-07-03 19:24:16 -0300

    Update views.py

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -19,22 +19,23 @@ def index(request):
     })
 
 
-#New Page: Clicking “Create New Page” in the sidebar should take the user to a page where they can create a new encyclopedia entry.
-#Users should be able to enter a title for the page and, in a textarea, should be able to enter the Markdown content for the page.
+#New Page: Clicking “Create New Page” in the sidebar should take the user to a page where they can create
+#a new encyclopedia entry.
+#Users should be able to enter a title for the page and, in a textarea, should be able to enter the
+#Markdown content for the page.
 #Users should be able to click a button to save their new page.
 #When the page is saved, if an encyclopedia entry already exists with the provided title, the user should be presented with an error message.
 #Otherwise, the encyclopedia entry should be saved to disk, and the user should be taken to the new entry’s page.
 
 def NewPage(request):
-    
-
-
-
-    if request.method == "GET":
-        return HttpResponse("Error. Wrong request method for New Page")
+    if request.method == "POST":
+        title = request.POST["NewTitle"]
+        content =request.POST["NewItem"]
+        return render(request, "encyclopedia/index.html", {"entries":util.save_entry(title, content)})
     else:
-        return HttpResponseRedirect(reverse("NewPage"))
+        return render(request, "encyclopedia/NewPage.html")
 
+        
 #Random Page: Clicking “Random Page” in the sidebar should take user to a random encyclopedia entry.
 
 def RandomPage(request):
