b8fdf94b284166ab324e0a90086fcbf32aab142a
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-07-06 18:16:59 -0300

    Update urls.py

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -8,12 +8,12 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("NewPage", views.NewPage, name="NewPage"),
     path("Search", views.Search, name="Search"),
-    path("EntryPage", views.EntryPage, name="EntryPage"),
     path("RandomPage", views.RandomPage, name="RandomPage"),
     path("EditPage", views.EditPage, name="EditPage"),
+    path("EntryPage", views.EntryPage, name="EntryPage"),
     path("AlertsDjango", views.AlertsDjango, name="AlertsDjango"),
-    path("<str:name>", views.Search, name="Search"),
-    path("<str:name>", views.EntryPage, name="EntryPage"),
-    path("CSS", views.EntryPage, name="EntryPage")
+    path("<str:pagename>", views.Search, name="EntryPage"),
+    path("<str:pagename>", views.EntryPage, name="EntryPage"),
+
 
 ]
