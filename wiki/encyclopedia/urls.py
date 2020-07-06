2941186b524404363849155db15eb4471e6b3bfb
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-07-06 14:53:27 -0300

    Update urls.py

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -6,10 +6,12 @@ app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
     path("NewPage", views.NewPage, name="NewPage"),
-    path("RandomPage", views.RandomPage, name="RandomPage"),
-    path("<str:name>", views.EntryPage, name="EntryPage"),
-    path("EditPage", views.EditPage, name="EditPage"),
+    path("<str:pagename>", views.Search, name="Search"),
     path("Search", views.Search, name="Search"),
+    path("<str:pagename>", views.EntryPage, name="EntryPage"),
+    path("Entrypage", views.EntryPage, name="EntryPage"),
+    path("RandomPage", views.RandomPage, name="RandomPage"),
+    path("EditPage", views.EditPage, name="EditPage"),
     path("AlertsDjango", views.AlertsDjango, name="AlertsDjango")
 
 ]
