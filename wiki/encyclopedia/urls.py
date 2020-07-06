72786ee82694bafa72cc9ef494fc6eea3a7acaeb
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-07-06 16:05:13 -0300

    Update urls.py

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -1,17 +1,18 @@
 from django.urls import path
 
+
 from . import views
 
 app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
     path("NewPage", views.NewPage, name="NewPage"),
-    path("<str:pagename>", views.Search, name="Search"),
     path("Search", views.Search, name="Search"),
-    path("<str:pagename>", views.EntryPage, name="EntryPage"),
-    path("Entrypage", views.EntryPage, name="EntryPage"),
+    path("EntryPage", views.EntryPage, name="EntryPage"),
     path("RandomPage", views.RandomPage, name="RandomPage"),
     path("EditPage", views.EditPage, name="EditPage"),
-    path("AlertsDjango", views.AlertsDjango, name="AlertsDjango")
+    path("AlertsDjango", views.AlertsDjango, name="AlertsDjango"),
+    path("<str:pagename>", views.Search, name="Search"),
+    path("<str:pagename>", views.EntryPage, name="EntryPage")
 
 ]
