d45ca01c174857ac81c7c55359f00cfc17115e23
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-07-09 14:14:02 -0300

    Update urls.py

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("NewPage", views.NewPage, name="NewPage"),
     path("wiki/<str:entry>", views.EntryPage, name="EntryPage"),
-    path("wiki/<str:Searchentry>", views.Search, name="EntryPage"),
     path("Search", views.Search, name="Search"),
     path("RandomPage", views.RandomPage, name="RandomPage"),
     path("EditPage", views.EditPage, name="EditPage")
