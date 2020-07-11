e3f7781c0a60c1388d6171027e264dcc889509ea
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-07-11 18:19:44 -0300

    Update views.py

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -15,27 +15,23 @@ class NewEntryForm(forms.Form):
     Econtent = forms.CharField(label="Econtent")
     entry = forms.CharField(label="entry")
 
-
+entry=""
 #Index Page return all itens from enciclopedia
 def index(request):
     if "Pwiki" not in request.session:
         request.session["Pwiki"] = []
-
+        request.session["Pwiki"] = "Pwiki"
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries() ,
         "encyclopedia": request.session["Pwiki"]
     })
 
 
-"""New Page: Clicking “Create New Page” in the sidebar should take the user to a page where they can create
-a new encyclopedia entry.
-Users should be able to enter a title for the page and, in a textarea, should be able to enter the
-Markdown content for the page.
-Users should be able to click a button to save their new page.
-when the page is saved, if an encyclopedia entry already exists with the provided title, the user should
- be presented with an error message.
-Otherwise, the encyclopedia entry should be saved to disk, and the user should be taken to the new entry’s
-page.
+"""
+   NewPage - falta:
+   Markdown content for the page.
+   when the page is saved, if an encyclopedia entry already exists with the provided title, the user should
+   be presented with an error message.
 """
 def NewPage(request):
     if request.method == "POST":
@@ -64,20 +60,12 @@ def NewPage(request):
         return render(request, "encyclopedia/NewPage.html")
 
 
-"""Entry Page: Visiting /wiki/TITLE, where TITLE is the title of an encyclopedia entry,
-should render a page that displays the contents of that encyclopedia entry.
-The view should get the content of the encyclopedia entry by calling the appropriate util function.
-If an entry is requested that does not exist, the user should be presented with an error page
-indicating that their requested page was not found.
-   If the entry does exist, the user should be presented with a page that displays the content of
-the entry
+"""
+   Entry Page: Falta:
+   Markdown to HTML Conversion: On each entry’s page, any Markdown content in the entry file should be
+   converted to HTML before being displayed to the user. You may use the python-markdown2 package
+   to perform this conversion, installable via pip3 install markdown2.
 
-  Markdown to HTML Conversion: On each entry’s page, any Markdown content in the entry file should be
-converted to HTML before being displayed to the user. You may use the python-markdown2 package
-to perform this conversion, installable via pip3 install markdown2.
-Challenge for those more comfortable: If you’re feeling more comfortable, try implementing the
-Markdown to HTML conversion without using any external libraries, supporting headings, boldface text,
-unordered lists, links, and paragraphs. You may find using regular expressions in Python helpful.
 """
 def EntryPage(request, entry):
     print("Estou na Entry Page")
@@ -88,76 +76,15 @@ def EntryPage(request, entry):
     html = markdown.markdown(content)
 
     context = {
-        "entry" :title.upper(),
+        "entry" :title,
         "pagename": pagename,
         "title": title,
         "content":html,
-        "page": page
+        "page": page,
+        "encyclopedia": request.session["Pwiki"]
         }
     return render(request, "encyclopedia/EntryPage.html", context)
 
-"""
-Edit Page: On each entry page, the user should be able to click a link to be taken to a page where
-the user can edit that entry’s Markdown content in a textarea.
-The textarea should be pre-populated with the existing Markdown content of the page.
-(i.e., the existing content should be the initial value of the textarea).
-The user should be able to click a button to save the changes made to the entry.
-Once the entry is saved, the user should be redirected back to that entry’s page.
-"""
-
-def EditPage(request):
-    if request.method == "POST":
-
-        Etitle= request.POST["entry"]
-        """
-        content = request.POST["content"]
-        Econtent = request.POST["Econtent"]
-
-        print("Estou no Post do Edit Page:" , Etitle)
-        print("Tentando salvar conteudo novo:", Econtent)
-        if len(Econtent) >0:
-            pagename = "Wiki/"+Etitle
-            filename =  f"entries/{Etitle}.md"
-            with open (filename, "w") as myfile:
-                myfile.seek(0,0)
-                myfile.write(Econtent)
-                myfile.save()
-
-                context={
-                     "page": "EntryPage",
-                     "pagename":pagename,
-                     "entry":title,
-                     "title":title,
-                     "content":content,
-                     "encyclopedia": request.session["Pwiki"]
-                }
-            return render(request, "encyclopedia/EntryPage.html", context)
-            """
-        context = {
-                "message":" Estou no Post do edit",
-                "entry":title
-        }
-
-        return render(request, "encyclopedia/EditPage.html",context)
-
-
-    #    else:
-    #        return render(request, "encyclopedia/EditPage.html",{"message":" The content is empty, please try again"})
-    else:
-        return render(request, "encyclopedia/EditPage.html" , {"message":"Empty Form"})
-
-
-"""
-Search: Allow the user to type a query into the search box in the sidebar to search
-for an encyclopedia entry.
-If the query matches the name of an encyclopedia entry, the user should be redirected
-to that entry’s page.
-If the query does not match the name of an encyclopedia entry, the user should instead
-be taken to a search results page that displays a list of all encyclopedia entries that
-have the query as a substring. For example, if the search query were Py, then Python
-should appear in the search results.
-Clicking on any of the entry names on the search results page should take the user to that entry’s page.
-"""
 
 def Search(request):
     if request.method == "POST":
@@ -180,7 +107,8 @@ def Search(request):
                 "pagename": pagename,
                 "title": title,
                 "message":message,
-                "content":util.get_entry(title=title)
+                "content":util.get_entry(title=title),
+                "encyclopedia": request.session["Pwiki"]
                 }
            return render(request, "encyclopedia/EntryPage.html", context)
 
@@ -200,7 +128,6 @@ def Search(request):
                         print(f"vou salvar para imprimir: ",titulo , str(count))
                         TitulosAchados.append(titulo)
 
-                    #    Searchentry=title.upper()
             if count == 0:
                 context ={
                     "count" : count,
@@ -213,19 +140,82 @@ def Search(request):
                     "seekword":seekword,
                     "count":count,
                     "titulo": titulo,
-                    "TitulosAchados": TitulosAchados             #list(sorted(TitulosAchados))
+                    "TitulosAchados": TitulosAchados,
+                    "encyclopedia": request.session["Pwiki"]
                      }
                 return render(request, "encyclopedia/Search.html", context)
-
-
-
-
     else:
         context ={
             "message":"Nao entrei no Post",
             "encyclopedia": request.session["Pwiki"]
             }
         return render(request, "encyclopedia/Search.html", context)
+
+
+"""
+Edit Page:  Falta:
+The textarea should be pre-populated with the existing Markdown content of the page.
+(i.e., the existing content should be the initial value of the textarea).
+The user should be able to click a button to save the changes made to the entry.
+Once the entry is saved, the user should be redirected back to that entry’s page.
+"""
+
+def EditPage(request):
+    print("Estou na  Edit Page:")
+    if request.method == "POST":
+        entry = request.POST["entry"]
+        title = request.POST["PassaTitulo"]
+        print("Estou no Post do Edit Page:" , title)
+
+        """
+        content = request.POST["content"]
+        Econtent = request.POST["Econtent"]
+
+
+        print("Tentando salvar conteudo novo:", Econtent)
+        if len(Econtent) >0:
+            pagename = "Wiki/"+Etitle
+            filename =  f"entries/{Etitle}.md"
+            with open (filename, "w") as myfile:
+                myfile.seek(0,0)
+                myfile.write(Econtent)
+                myfile.save()
+
+                context={
+                     "page": "EntryPage",
+                     "pagename":pagename,
+                     "entry":title,
+                     "title":title,
+                     "content":content,
+                     "encyclopedia": request.session["Pwiki"]
+                }
+            return render(request, "encyclopedia/EntryPage.html", context)
+            """
+        context = {
+                "message":" Estou no Post do edit",
+                "title": title,
+                "entry": entry,
+                "encyclopedia": request.session["Pwiki"]
+        }
+
+        return render(request, "encyclopedia/EditPage.html",context)
+
+
+    #    else:
+    #        return render(request, "encyclopedia/EditPage.html",{"message":" The content is empty, please try again"})
+    else:
+        context = {
+                "message":"Empty Form didn´t get into POST",
+        
+        }
+
+        return render(request, "encyclopedia/EditPage.html" ,context)
+
+
+
+
+
+
 
 
 
